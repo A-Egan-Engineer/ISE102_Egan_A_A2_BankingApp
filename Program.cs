f9f2d75aed3fb9a6a2d9e994d943487718088088
Andrew <aeganwork@gmail.com>
2025-11-03 18:18:44 +1100

    Initial Commit!

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleBankApp;
+    
+class Program
+{
+    static private string[] Username;
+    static void Main()
+    {
+        Console.WriteLine("Welcome to the Banking App");
+        Console.WriteLine("Are you a first time user?");
+        Console.ReadLine();
+    }
+}
