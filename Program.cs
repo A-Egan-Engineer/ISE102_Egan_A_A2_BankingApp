df7753375d9bb9d94d4e87d09691e9dc8d1e9a68
Andrew <aeganwork@gmail.com>
2025-11-03 19:29:37 +1100

    Initial sign-up feature commit!

--- Program.cs
+++ Program.cs
@@ -2,11 +2,37 @@
     
 class Program
 {
-    static private string[] Username;
     static void Main()
     {
+        // User greeted when opening application
         Console.WriteLine("Welcome to the Banking App");
+        // Confirming if user is a new or returning user
         Console.WriteLine("Are you a first time user?");
+        // Allowing user to make selection to sign in or register for app
+        Console.WriteLine("Enter: Y = Yes or N = No");
+        // Read users input for decision
+        string answerOne = Console.ReadLine();
+        // Decide outcome based on users input
+        if(answerOne == ("Y"))
+        {
+            // Insert Register Function Here
+        }
+        else if (answerOne == ("N"))
+        {
+            // Insert Sign In Function Here
+        }
         Console.ReadLine();
+    }
+
+    static void UserOption()
+    {
+        string option = Console.ReadLine();
+
+        switch (option)
+        {
+            case "New User":
+                Console.WriteLine("");
+                break;
+        }
     }
 }
