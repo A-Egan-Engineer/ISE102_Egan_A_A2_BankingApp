19481c64678e37678e70d6c1dab5abf7e46017d1
Andrew <aeganwork@gmail.com>
2025-11-05 14:49:58 +1100

    Final commit before merge!

--- obj/Debug/net8.0/ConsoleBankApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleBankApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleBankApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+789413bd0acf89ba01369bcd6b77f8e857af0653")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd221f79c9f6dce1eb9ac81322d8e298484027a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleBankApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleBankApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
