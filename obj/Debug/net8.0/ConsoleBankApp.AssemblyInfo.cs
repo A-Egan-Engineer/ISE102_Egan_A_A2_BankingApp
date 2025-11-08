4cd71a95f9350aa44a4360c4e23275f45cda77d6
Andrew <aeganwork@gmail.com>
2025-11-08 14:21:09 +1100

    Final Main Commit (Project Complete)

--- obj/Debug/net8.0/ConsoleBankApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleBankApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleBankApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71d158de760a2c365c725fb43de65dcc68fca3df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69e694632ca5d2d22e221e6453bdd3712b9cde50")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleBankApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleBankApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
