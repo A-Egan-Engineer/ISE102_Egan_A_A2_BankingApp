e4a3583c56b6c8429f2823aa52d77dc634b520ae
ae-dev <aeganwork@gmail.com>
2025-11-23 21:51:57 +1100

    Fixed CreditScore Method Updating creditScore Variable Every Call!

--- Classes/CreditCheck.cs
+++ Classes/CreditCheck.cs
@@ -1,31 +1,30 @@
-class CreditCheck : SignUpClass
+public class CreditCheck : SignUpClass
 {
-        public void CreditRating()
-    {
-        Console.WriteLine($"Welcome to your credit report {username}!\n");
-        
-        Random rand = new Random();
-        int creditScore = rand.Next(0,1200);
+    int creditScore = Random.Shared.Next(0, 1201);
 
-        if (creditScore == 0 && creditScore <= 459)
+        public void CreditScore()
+    {
+        Console.WriteLine($"Welcome to your credit score assessment {username}!\n");
+        
+        if (creditScore >= 0 && creditScore <= 459)
         {
-            Console.WriteLine($"Your credit score is {creditScore}. You have below average credit rating!");
+            Console.WriteLine($"Your credit score is {creditScore}. You have below average credit rating!\n");
         }
         else if (creditScore >= 460 && creditScore <= 660)
         {
-            Console.WriteLine($"Your credit score is {creditScore}. You have average credit.");
+            Console.WriteLine($"Your credit score is {creditScore}. You have average credit.\n");
         }
         else if (creditScore >= 661 && creditScore <= 734)
         {
-            Console.WriteLine($"Your credit score is {creditScore}. You have good credit.");
+            Console.WriteLine($"Your credit score is {creditScore}. You have good credit.\n");
         }
         else if (creditScore >= 735 && creditScore <= 852)
         {
-            Console.WriteLine($"Your credit score is {creditScore}. You have very good credit.");
+            Console.WriteLine($"Your credit score is {creditScore}. You have very good credit.\n");
         }
         else if (creditScore >= 853 && creditScore <= 1200)
         {
-            Console.WriteLine($"Your credit score is {creditScore}. You have excellent credit!");
+            Console.WriteLine($"Your credit score is {creditScore}. You have excellent credit!\n");
         }
     }
 }
